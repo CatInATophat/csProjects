7b7b2fb4a0a22212be9c668b30d648a880857307
Cat <118548392+CatInATophat@users.noreply.github.com>
2023-01-02 16:46:09 -0800

    Create Simple Input.cs
    Simple Input in C#

---
+++ Simple Input.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace firstCSproject
+{
+    
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            string testingForFun;
+            Console.Write("Enter a string: ");
+            testingForFun = Console.ReadLine();
+            Console.WriteLine("You entered: '{0}'", testingForFun);
+        }
+            
+    }
+}
